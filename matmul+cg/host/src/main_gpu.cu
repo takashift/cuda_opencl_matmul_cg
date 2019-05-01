6aabce3b46631e7f2d9a816f77cf3a70b2de90db
takashift <e145771@ie.u-ryukyu.ac.jp>
2019-05-02 01:32:29 +0900

    カーネルのスレッド、ブロックの指定は２次元にしないといけなかったので修正

--- matmul+cg/host/src/main_gpu.cu
+++ matmul+cg/host/src/main_gpu.cu
@@ -13,7 +13,7 @@ __global__ void matmul(float *a, float *b, float *c, unsigned long n) {
   float sum = 0.0f;
   if (i >= n || j >= n)
     return;
-    
+
   for(k=0; k<n; k++) {
     sum += a[i*n+k] * b[k*n+j];
   }
@@ -22,16 +22,16 @@ __global__ void matmul(float *a, float *b, float *c, unsigned long n) {
 
 void MatrixMultiplication_openmp(float *a,float *b, float *c, unsigned long N)
 {
-  int i, j, k ;
-  int chunk = N/4;
+  unsigned long  i, j, k ;
+  #ifdef _OPENMP
+	if(omp_get_thread_num() == 0) {
+    printf("Number of OpenMP threads %d\n", omp_get_num_threads());
+    int chunk = N/omp_get_num_threads();  
+	}
+  #endif
 
 #pragma omp parallel shared(a,b,c,chunk) private(i,j,k)
   {
-#ifdef _OPENMP
-	if(omp_get_thread_num() == 0) {
-		printf("Number of OpenMP threads %d\n", omp_get_num_threads());
-	}
-#endif
 #pragma omp for
     for (i=0; i<N; i++){
       for (j=0; j<N; j++)
@@ -50,7 +50,7 @@ void verify(float *h_c, float *c_CPU, unsigned long numdata_h) {
   double gpu_sum = 0.0;
   double rel_err = 0.0;
 
-  #pragma omp parallel for
+  // #pragma omp parallel for
   for (unsigned long i=0; i<numdata_h*numdata_h; i++){
     // std::cout << c_CPU[i] << "(CPU) " << std::endl;
     // std::cout << h_c[i] << "(GPU) " << std::endl;
@@ -131,7 +131,7 @@ int main(int argc, char *argv[]) {
   cudaMemcpy(d_a, h_a, numbyte, cudaMemcpyHostToDevice);
   cudaMemcpy(d_b, h_b, numbyte, cudaMemcpyHostToDevice);
   
-  matmul<<<numblock, numthread>>>(d_a, d_b, d_c, numdata_h);
+  matmul<<<dim3(numblock, numblock), dim3(numthread, numthread)>>>(d_a, d_b, d_c, numdata_h);
   
   cudaMemcpy(h_c, d_c, numbyte, cudaMemcpyDeviceToHost);
 
